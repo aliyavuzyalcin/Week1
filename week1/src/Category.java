2f71ea0ecdf667cbcbab137232009654d121d968
Yavuz <aliyavuzyalcin@gmail.com>
2021-05-10 01:33:29 +0300

    Week-1 on kodlama.io Java Camp

---
+++ week1/src/Category.java
@@ -0,0 +1,30 @@
+public class Category {
+
+
+    private String name;
+    private int id;
+
+    public Category() {
+    }
+
+    public Category(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name + "!";
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ week1/src/ProductManager.java
@@ -0,0 +1,9 @@
+import java.nio.file.FileSystemNotFoundException;
+
+public class ProductManager {
+
+    public void addToCart(Product product) {
+
+        System.out.println("Product added! " + product.getName());
+    }
+}
